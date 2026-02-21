973023d1e86a734a0eb126023c576b6cd9ac06a9
USER <sola201@naver.com>
2026-02-20 22:23:09 -0300

    Two Sum

---
+++ src/Arrays_Hashing/Two_Sum.java
@@ -0,0 +1,24 @@
+package Arrays_Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Two_Sum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indices = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            indices.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (indices.containsKey(diff) && indices.get(diff) != i) {
+                return new int[]{i, indices.get(diff)};
+            }
+        }
+
+        return new int[0];
+    }
+}
